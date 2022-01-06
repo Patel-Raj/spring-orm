9da32fa8267ba1ffb2dcb74fab7a64dda6ee2cc6
Raj Patel <patelraj22777@gmail.com>
2022-01-06 08:41:39 +0530

    Simple console based application

--- src/main/java/com/springorm/App.java
+++ src/main/java/com/springorm/App.java
@@ -5,6 +5,8 @@ import entites.Student;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -14,7 +16,71 @@ public class App
     public static void main( String[] args ) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
         StudentDao studentDao = applicationContext.getBean(StudentDao.class);
-        Student s = new Student(121,"Mohit","Jaipur");
-        studentDao.insert(s);
+        Scanner sc = new Scanner(System.in);
+        boolean ok = true;
+        while(ok) {
+            System.out.println("1 => Insert");
+            System.out.println("2 => Update");
+            System.out.println("3 => Delete");
+            System.out.println("4 => Search");
+            System.out.println("5 => Search All");
+            System.out.println("6 => Exit");
+            String choice = sc.next();
+            switch (choice) {
+                case "1":
+                    insertStudent(studentDao, sc);
+                    break;
+                case "2":
+                    updateStudent(studentDao, sc);
+                    break;
+                case "3":
+                    deleteStudent(studentDao, sc);
+                    break;
+                case "4":
+                    searchStudent(studentDao, sc);
+                    break;
+                case "5":
+                    System.out.println(studentDao.fetchAll());
+                    break;
+                case "6":
+                    ok = false;
+                    break;
+            }
+        }
+    }
+
+    private static void insertStudent(StudentDao studentDao, Scanner sc) {
+        System.out.println("Enter ID:");
+        int id = sc.nextInt();
+        System.out.println("Enter Name:");
+        String name = sc.next();
+        System.out.println("Enter City:");
+        String city = sc.next();
+        Student student = new Student(id, name, city);
+        studentDao.insert(student);
+    }
+
+    private static void updateStudent(StudentDao studentDao, Scanner sc) {
+        System.out.println("Enter ID:");
+        int id = sc.nextInt();
+        System.out.println("Enter Name:");
+        String name = sc.next();
+        System.out.println("Enter City:");
+        String city = sc.next();
+        Student student = new Student(id, name, city);
+        studentDao.update(student);
+    }
+
+    private static void deleteStudent(StudentDao studentDao, Scanner sc) {
+        System.out.println("Enter ID:");
+        int id = sc.nextInt();
+        studentDao.remove(id);
+    }
+
+    private static void searchStudent(StudentDao studentDao, Scanner sc) {
+        System.out.println("Enter ID:");
+        int id = sc.nextInt();
+        Student student = studentDao.fetch(id);
+        System.out.println(student);
     }
 }
--- src/main/java/dao/StudentDao.java
+++ src/main/java/dao/StudentDao.java
@@ -4,6 +4,8 @@ import entites.Student;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 public class StudentDao {
 
     private HibernateTemplate hibernateTemplate;
@@ -19,5 +21,24 @@ public class StudentDao {
     @Transactional
     public int insert(Student student) {
         return (int) hibernateTemplate.save(student);
+    }
+
+    @Transactional
+    public void remove(int id) {
+        Student student = fetch(id);
+        hibernateTemplate.delete(student);
+    }
+
+    @Transactional
+    public void update(Student student) {
+        hibernateTemplate.update(student);
+    }
+
+    public Student fetch(int id) {
+        return hibernateTemplate.get(Student.class, id);
+    }
+
+    public List<Student> fetchAll() {
+        return hibernateTemplate.loadAll(Student.class);
     }
 }
